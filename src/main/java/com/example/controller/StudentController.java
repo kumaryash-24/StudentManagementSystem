b6367b59caac50f3e0f63cdcf2668bedbe192aa4
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-09-16 10:24:22 +0530

    Update StudentController.java

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;            
 import java.sql.SQLException;              
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.ArrayList;             
 import java.util.List;            
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
