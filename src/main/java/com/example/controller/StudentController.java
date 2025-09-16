8addc6f6ff4a64c55c24db3e4f4f87e517dc57a5
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-09-16 10:23:54 +0530

    Update StudentController.java

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.ResultSet;            
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
